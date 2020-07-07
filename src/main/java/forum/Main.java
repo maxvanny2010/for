9e9c4cdf1cbe43cdec60f73906a6a64b169a9feb
Maxim <mailtime2010@gmail.com>
2020-07-07 10:48:30 -0400

    5. Boot 0. Spring Boot SD[6880#301788]

--- src/main/java/forum/Main.java
+++ src/main/java/forum/Main.java
@@ -1,9 +1,14 @@
 package forum;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 /**
  * Main.
@@ -14,6 +19,20 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
+    @Override
+    protected final SpringApplicationBuilder configure(
+            final SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(@Qualifier("dataSource") final DataSource ds) {
+        final SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
+
     /**
      * Method a pointer to program.
      *
@@ -22,11 +41,4 @@ public class Main extends SpringBootServletInitializer {
     public static void main(final String[] args) {
         SpringApplication.run(Main.class);
     }
-
-    @Override
-    protected final SpringApplicationBuilder configure(
-            final SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
-
 }
